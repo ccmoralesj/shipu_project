aa16da0d226d5c254337b297b7e514b4acb4e34a
Cristian Camilo Morales <ccmj202@gmail.com>
2018-02-19 16:26:36 -0500

    style(Error Handler): Code style refactor

--- src/helper/errorHandler.js
+++ src/helper/errorHandler.js
@@ -1,9 +1,9 @@
 
 module.exports = (ctx, error, status) => {
-    ctx.status = error.status || status || 500;
-    const errorResponse = {};
-    errorResponse.status = error.status || 500;
-    errorResponse.message = error.message || error;
-    ctx.body = errorResponse;
-    ctx.app.emit('error', error, ctx);
+  ctx.status = error.status || status || 500;
+  const errorResponse = {};
+  errorResponse.status = error.status || 500;
+  errorResponse.message = error.message || error;
+  ctx.body = errorResponse;
+  ctx.app.emit('error', error, ctx);
 };
